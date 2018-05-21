4cd6b96573ddb85288c3d9b28c0ca9efd5102174
sobayashi <sophieobayashi@gmail.com>
2018-05-20 18:59:59 -0700

    Added comments

--- liri.js
+++ liri.js
@@ -12,8 +12,8 @@ const omdb = require('omdbapi')
 var req = process.argv[2];
 var logData;
 var q = "";
-var movieq = "";
 
+// In case forget to put query in quotes in terminal
 for (i = 3; i < process.argv.length; i++) {
 	q += (" " + process.argv[i]);
 }
@@ -41,23 +41,37 @@ switch (req) {
 
 /* MAIN COMPONENTS */
 
-// 1. SPOTIFY API
+// Append data to log.text
+function writeToLog() {
+    fs.appendFile("log.txt", logData, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 
+
+/* 1. SPOTIFY API */
 function spotifyThis () { 
 
-    if (!q) {
+    // If empty string, default to
+    if (!q) {  
         q = "The Sign Ace of Base";
     }
 
-    var spotify = new Spotify({
+    // User authentification    
+    var spotify = new Spotify({     
         id: process.env.SPOTIFY_ID,
         secret: process.env.SPOTIFY_SECRET
     });
 
+    // Search Spotify
     spotify.search({ type: 'track', query: q }, function(err, data) {
-        if (err) {
+        if (err) {  // Return error
             return console.log('Error occurred: ' + err);
-        } else {
+        } else { 
+
+            // Log spotify components
             for (var i = 0; i < data.tracks.items.length; i++){
             console.log("Artist: " + data.tracks.items[i].artists[0].name)
             console.log("Song: " + data.tracks.items[i].name)
@@ -65,37 +79,37 @@ function spotifyThis () {
             console.log("Listen on Spotify: " + data.tracks.items[i].external_urls.spotify)
             console.log("---")
 
+            // Data to append to log.txt file
             logData = "Artist: " + data.tracks.items[i].artists[0].name + "\n" 
                     + "Song: " + data.tracks.items[i].name + "\n" 
                     + "Album: " + data.tracks.items[i].album.name + "\n" 
                     + "Listen on Spotify: " + data.tracks.items[i].external_urls.spotify + "\n"
                     + "---" + "\n";
-           
-            fs.appendFile("log.txt", logData, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-
+            
+            // Append logData
+            writeToLog();
 
             }
         }
     });
-
 }
 
 
 
-// 2. OMDB API
+/* 2. OMDB API */
 function omdbThis () {
 
-    if (!q) {
+    if (!q) {    // Return error
         q = "Mr.+Nobody";
     }
 
+    // Constructing query
     var queryUrl = "http://www.omdbapi.com/?t=" + q + "&y=&tomatoes=true&plot=short&apikey=" + process.env.OMDB_KEY;
 
+    // npm HTTP request
     request(queryUrl, function(error, response, body) {
+
+        //Log movie components
         if (!error && response.statusCode === 200) {
             console.log("Title: " + JSON.parse(body).Title);
             console.log("Release Year: " + JSON.parse(body).Year);
@@ -106,6 +120,7 @@ function omdbThis () {
             console.log("Plot Summary: " + JSON.parse(body).Plot);
             console.log("-------------------------------------------------");
         
+            // Data to append to log.txt file
             logData = "Title: " + JSON.parse(body).Title + "\n"
             + "Release Year: " + JSON.parse(body).Year + "\n" 
             + "IMDb Rating: " + JSON.parse(body).imdbRating  + "\n" 
@@ -115,19 +130,16 @@ function omdbThis () {
             + "Plot Summary: " + JSON.parse(body).Plot + "\n" 
             + "-------------------------------------------------" + "\n";
 
-            fs.appendFile("log.txt", logData, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+            // Append logdata
+            writeToLog();
+
         }
     });
 };
 
 
 
-
-// 3. TWITTER
+/* 3. TWITTER */
 function tweetThis () {
     var client = new Twitter({
       consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -138,36 +150,37 @@ function tweetThis () {
     
     var params = {user_id: '997227706038145030'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-            console.log(tweets[i].text);
+     
+        // log tweets
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
 
-            logData = tweets[i].text + "\n";
+                // Data to append to log.txt file
+                logData = tweets[i].text + "\n"; 
 
-            fs.appendFile("log.txt", logData, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+                // Append logData
+                writeToLog();
 
-
+            }
         }
-      }
     });
     console.log("-------------------------------------------------")
 }
 
-// 4. Do What It Says
+/* 4. DO WHAT IT SAYS */
 function doThis () {
     fs.readFile("random.txt", "utf8", function(err, data) {
         if (err) {
             return console.log(err);
         }
         
+        // Reassign q & req variables to data from random.txt
         var output = data.split(",");
         q = output[1];
         req = output[0];
         
+        // New switch case for new q & req values
         switch (req) {
             case "spotify-this-song":
                 spotifyThis();
